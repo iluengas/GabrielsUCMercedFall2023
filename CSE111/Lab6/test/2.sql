b2b893614dfab776840eb34d292c853b4e4afc8c
Gabriel <gabeben3@gmail.com>
2023-10-09 15:02:22 -0700

    modified:   CSE111/Lab6/test/2.sql 	modified:   CSE111/Lab6/test/3.sql

--- CSE111/Lab6/test/2.sql
+++ CSE111/Lab6/test/2.sql
@@ -2,14 +2,16 @@
 -- 2. Find how many distinct customers have at least one order supplied
 --      exclusively by suppliers from AFRICA.
 
-SELECT DISTINCT *--COUNT(DISTINCT c_custkey) as customer_cnt
+SELECT DISTINCT l_orderkey--COUNT(DISTINCT c_custkey) as customer_cnt
 FROM lineitem, 
 (
-    SELECT DISTINCT l_suppkey as suppKey
+    SELECT DISTINCT s_suppkey as suppKey
     FROM lineitem, supplier, nation, region
     WHERE l_suppkey = s_suppkey AND 
             s_nationkey = n_nationkey AND 
                 n_regionkey = r_regionkey AND 
-                    r_name != 'AFRICA'
-) 
+                    r_name IN ('AFRICA')
+)
 WHERE l_suppkey != suppKey;
+
+
--- CSE111/Lab6/test/3.sql
+++ CSE111/Lab6/test/3.sql
@@ -1,3 +1,22 @@
 .headers on
 -- 3. Find the distinct parts (p name) ordered by customers from ASIA
 --  that are supplied by exactly 3 suppliers from AFRICA.
+
+SELECT DISTINCT p_name 
+FROM (
+    SELECT p_partkey as pKey
+    FROM part, partsupp, supplier, nation, region
+    WHERE p_partkey = ps_partkey AND 
+            ps_suppkey = s_suppkey AND 
+                s_nationkey = n_nationkey AND 
+                    n_regionkey = r_regionkey AND 
+                        r_name IN ('AFRICA')
+                                GROUP BY p_partkey 
+                                    HAVING COUNT(p_partkey) = 3
+), lineitem, part, orders, customer, nation, region
+WHERE l_partkey = pKey AND 
+        l_orderkey = o_orderkey AND 
+            o_custkey = c_custkey AND 
+                c_nationkey = n_nationkey AND 
+                    n_regionkey = r_regionkey AND
+                        r_name IN ('ASIA');
