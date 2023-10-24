4e1ed9086ce044c67064e980121b17e997684fa4
Gabriel Benavidez <gabeben3@gmail.com>
2023-10-24 11:21:23 -0700

    new file:   CSE106/Lab7 (2).zip 	modified:   CSE106/Lab7/lab7.py 	modified:   CSE106/Lab7/static/index.js

--- CSE106/Lab7/lab7.py
+++ CSE106/Lab7/lab7.py
@@ -3,15 +3,26 @@ from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+#Configure Flask app
 app = Flask(__name__)
 CORS(app)
+
+#Configure DB
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite" 
+
+#set db variable as a SQLAlchemy obj tied to flask app "app"
 db = SQLAlchemy(app) 
 
+
+#Enter schema for all tables in db
+        #Interpreter will view this "table" as an object with various attributes 
 class Gradebook(db.Model):
         name = db.Column(db.String, primary_key=True, unique=True, nullable=False)
         grade = db.Column(db.Integer, nullable=False)
 
+
+#Funtion to convert SQL quereies (received as objects) to dictionaries we can parse
+        #Recieves array of objects as result for SQL query with multiple lines, each line is an object in the array 
 def showGradebookSelection(queryArr):
         returnDict = {}
 
@@ -20,53 +31,93 @@ def showGradebookSelection(queryArr):
 
         return (returnDict)
 
+#Flask app structure: 
+        #index.js (Front-end) file includes all fetch requests we make to out server 
+                #Includes fetch functions that receive JSON responses from the server and manipulates it however 
+                        #In This case, the JSON responses are converted into HTML we append to index.html
+        #lab7.py (Back-End) file acts as our server and contains all of the routes that determine what kind of response will be sent out based on request information
+                #In this case, the server routes expect a few types of requestes
+                        #GET: lets us retrieve data from our db, no request parsing needed
+                        #<student_name> GET: we can send a variable through a url from a front-end request, we can use this variable however we like
+                        #POST (w/request data): Post lets us add data to our DB
+                                #We can intake the request data coming in as JSON and parse it with json.loads(request.data)
+                        #PUT: Used when we want to manipulate/edit existing data 
+                        #DELETE: Delete data from table 
+
+
+#Default Route "Landing page"
 @app.route('/')
 def renderIndex():
+        #Display html file (**All html's go in templates -> CSS & JS go in static)
         return render_template("index.html")
 
+#/grades route - method=GET
+#Retrieves all rows in table in db
 @app.route('/grades', methods=['GET'])
 def getGrades():
 
+        #Query all rows in Gradebook table
+        # # Returns an array of objects     
         queryArr = (Gradebook.query.all())
 
+        #Parse array of objects into a dictionary 
+                #jsonify dictionary and send it as a JSON response to the front end  
         return jsonify(showGradebookSelection(queryArr))
 
+#/grades/<student_name> route
+# Gets a specific row in db matching the <student_name> variable in the URL
 @app.route('/grades/<student_name>', methods=['GET'])
-def getStudentGrade(student_name):
+def getStudentGrade(student_name):      #include variables in function arguments 
 
+        #Use Query.filter by -> followed by whatever we cant our filter to be **Can be algebraic 
+                #Student is returned as a Gradebook obj
         student = Gradebook.query.filter_by(name = student_name).first()
 
+        #Initilize a dactionary to return our response through 
         returnDict = {}
+
+        #Populate dictionary with data from Gradebook Obj
         returnDict[student.name] = student.grade
 
+        #Return json if query was not empty 
         if student:
                 return jsonify(returnDict)
         else:
                 return
 
 
+#/Grades, method=POST
 @app.route('/grades', methods=['POST'])
 def createSutdent():
-
+        #Request method conditional **needed
         if request.method == "POST":
 
+                #Load data from request in a json obj
                 data = json.loads(request.data)
 
+                #Parse request data into variables
                 newStudentName = data['name']
                 newStudentGrade = data['grade']
 
+                #With statement needed to make alterations to db 
                 with app.app_context():
+
+                        #Create new Gradebook object with value arguments
                         newStudent = Gradebook(name = newStudentName, grade=newStudentGrade)
 
+                        #Add new object to db
                         db.session.add(newStudent)
 
+                        #Commit changes to db
                         db.session.commit()
 
                         print("added student")
 
+                        #Return all GradeBook rows as json to front end 
                         return jsonify(showGradebookSelection(Gradebook.query.all()))
 
-
+#/grades/<student_name>, method=DELETE Route
+#Deletes a row from db given a name through URL
 @app.route('/grades/<student_name>', methods=['DELETE'])
 def deleteSutdent(student_name):
         
@@ -83,21 +134,31 @@ def deleteSutdent(student_name):
                         else:
                                return
 
-
+#/grades/<student_name>, method=PUT
+#Edit row in db based on name received through URL
 @app.route('/grades/<student_name>', methods=['PUT'])
 def editSutdentGrade(student_name):
         
         if request.method == "PUT":
+
+                #Parse data received through request 
+                        #Name
+                        #New Grade
                 data = json.loads(request.data)
 
+                #Assign variable to value of grade in request.
                 newGrade = data['grade']
 
+                #Query the Gradebook table for the first entry where the name matches the student_name argument 
                 student = Gradebook.query.filter_by(name = student_name).first()
 
+                #Assign the newGrade value from request data to the student object's grade attribute 
                 student.grade = newGrade
 
+                #Commit Changes
                 db.session.commit()
 
+                #Return whole table as JSON to front end 
                 return jsonify(showGradebookSelection(Gradebook.query.all()))
 
 if __name__ == '__main__':
--- CSE106/Lab7/static/index.js
+++ CSE106/Lab7/static/index.js
@@ -1,6 +1,13 @@
+
+//Function that intakes json and parses it into html to add to table
+    //Use async functions so page can handle other operations while awaiting a response from server 
+    //Not necessary, but could improve page speed and usability 
+    //If an async function is used, all functions must also be async
 async function displayData(responseData) {
 
+    //Await response data 
     const grades = await responseData.json();
+    //Log response data 
     console.log(grades);
 
     let placehold = document.querySelector("#grades");  //Selects field with grades id 
@@ -20,10 +27,14 @@ async function displayData(responseData) {
     placehold.innerHTML = x;  //plug new rows and data into table 
 }
 
+//Request to fetch all grades in db
 async function fetchAllGrades() {
+
+    //Await fetch response (make sure url matches local host and route we want to use in backend), define method 
     let response = await fetch('http://127.0.0.1:5000/grades', { method: 'get', mode: 'cors' });
 
 
+    //Error handling 
     if (response.ok) { // if HTTP-status is 200-299
         console.log(response);
         
@@ -31,15 +42,21 @@ async function fetchAllGrades() {
         alert("HTTP-Error: " + response.status);
     }
 
+    //Send response data json to displayFunction to print it to the page 
     displayData(response);
 }
 
+
+//Function to fetch a student grade based on name received through name field on front-end html
 async function fetchStudentGrade(name) {
+
+    //Await fetch response -> plug name argument variable into url to send var to backend route  
     let response = await fetch('http://localhost:5000/grades/'+name, {
         method: "GET",
         mode: 'cors'
     });
 
+        //Error handling 
     if (response.ok) { // if HTTP-status is 200-299
         console.log(response);
         
@@ -49,11 +66,18 @@ async function fetchStudentGrade(name) {
     displayData(response);
 }
 
+
+//Function to delete student from db
+    //Receive name variable from field in front-end
 async function fetchDeleteStudentGrade(name) {
+
+    //await fetch request with name variable plugged into URL
     let response = await fetch('http://localhost:5000/grades/'+name, {
         method: "DELETE",
         mode: 'cors'
         });
+
+        //Error handling 
     if (response.ok) { // if HTTP-status is 200-299
         console.log(response);
         alert("Successfully Deleted Grade");
@@ -66,23 +90,28 @@ async function fetchDeleteStudentGrade(name) {
 }
 
 
+//Function to create a new entry in our DB 
+    //Name and grade vars from HTML input field
 async function fetchCreateGrade(name, grade) {
-    // var x = document.getElementById("newStudentName").value;
-    // var grade = document.getElementById("newStudentGrade").value;
 
+    //Create data dictionary with data from our argument variabes 
     const data = {"name": name, "grade":grade};
 
+    //Log data being sent 
     console.log(data)
     
+    //Create request with data in to body to send to back-end 
     let response = await fetch('http://localhost:5000/grades', {
         method: "POST",
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", 
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json', //Define what kind of data we are including in request body 
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data), //Send JSON data through the body of the request 
     });
+
+    //Error Handling 
 
     if (response.ok) { // if HTTP-status is 200-299
         console.log(response);
@@ -95,11 +124,19 @@ async function fetchCreateGrade(name, grade) {
 
 }
 
+
+//Function to edit a grade in the gradebook
+    //newName and newGrade vars are from HTML input field 
 async function fetchEditGrade(name, newGrade) {
+
+    //Initialize and define data dict 
     const data = {"grade": newGrade};
 
+    //Console log data dict
     console.log(data);
 
+    //Create/await a fetch request 
+        //Inlcude name in URL path to send as a variable 
     let response = await fetch('http://localhost:5000/grades/'+name, {
         method: "PUT",
         mode: "cors", // no-cors, *cors, same-origin
@@ -107,7 +144,7 @@ async function fetchEditGrade(name, newGrade) {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data), //Json stringify data dict to send to back end 
     });
 
     if (response.ok) { // if HTTP-status is 200-299
