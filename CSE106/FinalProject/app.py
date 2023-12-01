9bd4c2abf6a2e680c647c34bb163614a07d46aa2
Gabriel <gabeben3@gmail.com>
2023-12-01 04:39:38 -0800

    deleted:    CSE106/FinalProject/SQLite_Python.db 	modified:   CSE106/FinalProject/app.py 	modified:   CSE106/FinalProject/instance/main.db 	new file:   CSE106/FinalProject/instance/queryTestSpace.sql 	deleted:    CSE106/FinalProject/templates/added.html 	modified:   CSE106/FinalProject/templates/allUsers.html 	new file:   CSE106/FinalProject/templates/createPost.html 	new file:   CSE106/FinalProject/templates/frontPage.html 	renamed:    CSE106/FinalProject/templates/createUser.html -> CSE106/FinalProject/templates/login_profile/createUser.html 	renamed:    CSE106/FinalProject/templates/login.html -> CSE106/FinalProject/templates/login_profile/login.html 	renamed:    CSE106/FinalProject/templates/signUpForm.html -> CSE106/FinalProject/templates/login_profile/signUpForm.html 	deleted:    CSE106/FinalProject/templates/success.html 	deleted:    CSE106/FinalProject/test.sql 	deleted:    CSE106/FinalProject/test3.sql

--- CSE106/FinalProject/app.py
+++ CSE106/FinalProject/app.py
@@ -76,31 +76,21 @@ def closeConnection(_conn, _dbFile):
         print(e)
 
 
-
-
-
 #Landing Page route, will go to login page
 @app.route('/')
-def renderIndex():
-        
-        # sql = """SELECT * 
-        #          FROM Posts """
-        
-        # #Connect to DB **Necessary before each query 
-        # conn = openConnection(database)
-        # cur = conn.cursor()
-
-        # #Execute query and save all rows to a variable
-        # cur.execute(sql)
-        # rows = cur.fetchall()
-
-        # print(rows)
-
+def home():
         #Display html file (**All html's go in templates -> CSS & JS go in static)
-        return render_template("login.html")
+        return redirect(url_for('redirectFrontPage'))
+
+#Landing Page route, will go to login page
+@app.route('/redirectLoginPage')
+def redirectLoginPage():
+        #Display html file (**All html's go in templates -> CSS & JS go in static)
+        return render_template("login_profile/login.html")
+
 
 # #Route to handle login
-    #Routes to /success/<name> on successful login
+    #Routes to /redirectFrontPage on successful login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
@@ -109,7 +99,7 @@ def login():
     _password = request.form['password']
 
     if current_user.is_authenticated:
-        return redirect(url_for('success',name = _username))
+        return redirect(url_for('redirectFrontPage'))
     user = Logins.query.filter_by(email=_username).first()
     if user is None or not user.check_password(_password):
         error = "Invalid Username or Password"
@@ -118,11 +108,11 @@ def login():
     else:
         login_user(user)
         next_page = request.args.get('next')
-        return redirect(url_for('success',name = _username))
+        return redirect(url_for('redirectFrontPage'))
 
 @app.route('/signUp')
 def signUp():
-    return render_template('signUpForm.html')
+    return render_template('login_profile/signUpForm.html')
 
 # #Route to let current user log out 
     #Redirects to landing/login page
@@ -132,6 +122,8 @@ def logout():
     logout_user()
     return render_template("login.html")
 
+#Recieves data from createUser.html- Updates Users table with new profile data 
+    #Redirects back to homepage
 @app.route("/createUser", methods=['GET', 'POST'])
 @login_required
 def createUser():
@@ -141,33 +133,36 @@ def createUser():
         _bio = request.form['bio']  
         _img = request.files['profilePicture']  
 
+        #Type in raw Sql
         sql = """INSERT INTO Users(user_id, username, bio,
                         profile_picture, creation_date, views)
                     VALUES(?, ?, ?, ?, datetime('now', 'localtime'), 0);"""  
-
-        conn = openConnection(database)
-
         
+
+        #Get User ID
         _id = Logins.get_id(current_user)
-        print("ID" + str(_id))
-        # user = db.session.query(Logins).get(_id)
+        # print("ID" + str(_id))
 
-        # print("USER" + user)
-
-# Encode the file content as Base64
+        # Encode the file content as Base64
         encoded_image = base64.b64encode(_img.read()).decode('utf-8')
 
+        #Define the arguments
         args = [_id, _username, _bio, encoded_image]
+
+        #Connect to DB and execute Sql -> Inputs new User to UserTable based on new account Creation
+        conn = openConnection(database)
         conn.execute(sql, args)
         conn.commit()
     
-        return render_template("added.html")
-        #Execute query and save all rows to a variable
-    
+        return redirect(url_for('redirectFrontPage'))
+
+#Receives data from Login.hmtl to create an account on the site
+# #Redirects by default to createUser.html    
 @app.route('/createAccount', methods=['POST'])
 def createAccount():
+     
      if request.method == 'POST':
-        _email = request.form['newName']
+        _email = request.form['newEmail']
         newPassword = request.form['newPassword']
 
         newPassword = bcrypt.generate_password_hash(newPassword).decode('utf-8') 
@@ -185,33 +180,84 @@ def createAccount():
             #Commit changes to db
             db.session.commit()
 
-            print("added student")
+            print("added accoutn")
 
         # print (_email + newPassword)
 
         user = Logins.query.filter_by(email=_email).first()
         login_user(user)
 
-        return render_template("createUser.html")
+        # Redirects new users to create a profile
+        return render_template("login_profile/createUser.html")
 
+#Call this route to display all of the users 
+    #allUsers will come as a tuple of tuples, including all data from Users table
 @app.route("/displayAllUsers")
 def display():
+
+    #Type in raw sql 
     sql = """SELECT * FROM USERS"""
 
+    #Connect to the database and cursor 
+    conn = openConnection(database)
+    cur = conn.cursor()
+
+    #Execute query and save all rows to a variable
+    cur.execute(sql)
+    _allUsers = cur.fetchall()
+
+    #Render the html page allUsers.html, page will recieve tuple of tuples as a variable and can be parsed through
+    return render_template("allUsers.html", allUsers = _allUsers)
+
+#Call this route to redirect to the front Page
+    #postData will come as a tuple of tuples, including all data from Posts table
+@app.route("/redirectFrontPage")
+def redirectFrontPage():
+    sql = """SELECT postID, username, post_textContent, post_imageContent, post_creationDate, post_likes, post_dislikes 
+            FROM Posts, Users WHERE post_userID = user_id"""
+    
     conn = openConnection(database)
     cur = conn.cursor()
 
         #Execute query and save all rows to a variable
     cur.execute(sql)
-    _allUsers = cur.fetchall()
+    _postData = cur.fetchall()
 
-    return render_template("allUsers.html", allUsers = _allUsers)
+    if current_user.is_authenticated:
+        return render_template("frontPage.html", postData = _postData, loggedIn = 1)
+    else:
+        return render_template("frontPage.html", postData = _postData)
+    
+#Call this route to redirect to the create Post Page
+    #tags will come as a tuple of tuples, including all data from tags table. This will be used to dynamically change the form
+@app.route("/redirectCreatePost")
+def redirectCreatePost():
+    sql = """SELECT * 
+                FROM Tags"""
+    
+    conn = openConnection(database)
+    cur = conn.cursor()
 
-@app.route("/success/<name>")
-@login_required
-def success(name):
-    _id = Logins.get_id(current_user)
-    return render_template("success.html", name = _id)
+        #Execute query and save all rows to a variable
+    cur.execute(sql)
+    _tags = cur.fetchall()
+    return render_template("createPost.html", tags = _tags)
+
+
+#Route to confirm post creation -> Updates Posts table with form data from createPost.html
+    #will redirect to front page
+        ### WORK IN PROGRESS ###
+@app.route("/createPost", methods = ['GET', 'POST'])
+def createPost():
+    if request.method == 'POST':
+        _textContent = request.form['textContent'] 
+        _imageContent = request.files['imageContent']
+
+        postTags = request.form.getlist('postTags')
+        print(_textContent)
+        print(postTags)
+
+    return render_template("/redirectCreatePost.html")
 
 
 
---
+++ CSE106/FinalProject/instance/queryTestSpace.sql
@@ -0,0 +1,42 @@
+
+-- CREATE TABLE Posts (
+--     postID            INTEGER PRIMARY KEY
+--                               NOT NULL,
+--     post_userID       INTEGER,
+--     post_textContent  TEXT,
+--     post_imageContent BLOB,
+--     post_creationDate  TEXT,
+--     post_likes  INTEGER,
+--     post_dislikes   INTEGER,
+--     parent_postID   INTEGER,
+--     FOREIGN KEY (parent_postID) REFERENCES Posts(postID),
+--     FOREIGN KEY (post_userID) REFERENCES Users(user_id)
+-- );
+
+-- SELECT * FROM Posts WHERE parent_postID = 1;
+
+-- CREATE TABLE PostTags (
+--     rowID INTEGER PRIMARY KEY NOT NULL,
+--     postID INT,
+--     tagID INT,
+--     FOREIGN KEY (postID) REFERENCES Posts(postID),
+--     FOREIGN KEY (tagID) REFERENCES Tags(tagID)
+-- );
+
+-- Functions to inssert Data into posts and Users
+
+-- INSERT INTO Users(user_id, username, bio,
+--                      profile_picture, creation_date, views)
+-- VALUES(?, ?, ?, ?, datetime('now', 'localtime'), 0);
+
+-- INSERT INTO Posts(postID, post_userID,
+--                  post_textContent, post_imageContent,
+--                  post_creationDate, post_likes,
+--                  post_dislikes, parent_postID)
+-- VALUES(5, 3, 'Insertion DataTime test', NULL, datetime('now', 'localtime'), 0, 0, NULL );
+
+
+-- Funtions to attatch usernames belonging to a post's user to the postQuery (replacing the post_userID column)
+SELECT postID, username, post_textContent, post_imageContent, post_creationDate, post_likes, post_dislikes 
+FROM Posts, Users 
+WHERE post_userID = user_id;
